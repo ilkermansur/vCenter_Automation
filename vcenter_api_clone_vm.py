e595500bbbd9160982366e09196f2d7c3be3f0c3
ilkermansur <59640302+ilkermansur@users.noreply.github.com>
2024-04-05 15:23:31 +0300

    Add files via upload

--- vcenter_api_clone_vm.py
+++ vcenter_api_clone_vm.py
@@ -7,6 +7,15 @@ Developer: ilker MANSUR
 Email: imansur@morten.com.tr
 GitHub: github.com/ilkermansur
 
+Arguments:
+
+dst_folder_name = ""            # Destination folder name
+dst_datastore_name = ""         # Destination datastore name
+dst_host_ip = ""                # Destination host IP
+dst_resource_pool_name = ""     # Destination resource pool name
+src_vm_name = ""                # Source VM name
+dst_vm_name = ""                
+ 
 """
 
 import requests
@@ -53,23 +62,111 @@ if session_id is not None:
 else:
     print("Session ID not found")
 
-###
+############################################################################################################
+dst_folder_name = "IM-POD-VM-01"
+dst_datastore_name = "LAB-1"
+dst_host_ip = "192.168.49.21"
+dst_resource_pool_name = "IM-POD-VM-01"
+src_vm_name = "HQ-CUCM_14.0"
+dst_vm_name = "TEST-API-VM"
+############################################################################################################
+
+# Find VM ID
+
+vm_list_url = f"https://{vcenter_ip}/api/vcenter/vm"
+
+response = requests.get(url=vm_list_url, headers={"vmware-api-session-id": session_id}, verify=False)
+if response.status_code == 200:
+    vm_list = response.json()
+    for vm in vm_list:
+        if src_vm_name == vm["name"]:
+            src_vm_id = vm["vm"]
+        else:
+            continue
+else:
+    print(f"Failed to get VM list. Status code: {response.status_code}")
+
+# Find Folder ID
+
+folder_url = f"https://{vcenter_ip}/api/vcenter/folder"
+response = requests.get(url=folder_url, headers={"vmware-api-session-id": session_id}, verify=False)
+if response.status_code == 200:
+    folder_list = response.json()
+    for folder in folder_list:
+        if dst_folder_name == folder["name"]:
+            folder_id = folder["folder"]
+        else:
+            continue
+else:
+    print(f"Failed to get folder list. Status code: {response.status_code}")
+
+# Find Datastore ID
+
+datastore_list_url = f"https://{vcenter_ip}/api/vcenter/datastore"
+
+response = requests.get(url=datastore_list_url, headers={"vmware-api-session-id": session_id}, verify=False)
+if response.status_code == 200:
+    datastore_list = response.json()
+    for datastore in datastore_list: 
+        if dst_datastore_name == datastore["name"]:
+            datastore_id = datastore["datastore"]
+        else:
+            continue
+else:
+    print(f"Failed to get datastore list. Status code: {response.status_code}")
+    
+# Find Host ID
+
+host_list_url = f"https://{vcenter_ip}/api/vcenter/host"
+
+response = requests.get(url=host_list_url, headers={"vmware-api-session-id": session_id}, verify=False)
+if response.status_code == 200:
+    host_list = response.json()
+    for host in host_list:
+        if dst_host_ip == host["name"]:
+            host_id = host["host"]
+        else:
+            continue
+else:
+    print(f"Failed to get host list. Status code: {response.status_code}")
+
+# Find Resource Pool ID
+    
+resource_pool_url = f"https://{vcenter_ip}/api/vcenter/resource-pool"
+
+response = requests.get(url=resource_pool_url, headers={"vmware-api-session-id": session_id}, verify=False)
+if response.status_code == 200:
+    rp_list = response.json()
+    for rp in rp_list:
+        if dst_resource_pool_name == rp["name"]:
+            resource_pool_id = rp["resource_pool"]
+        else:
+            continue
+else:
+    print(f"Failed to get resource pool list. Status code: {response.status_code}")
+            
+############################################################################################################
 clone_url = f"https://{vcenter_ip}/api/vcenter/vm?action=clone"
 
 # Use 'ID' instead of 'name' for the source VM
 json_data ={
-    "name" : "test-api-clone",
-    "source" : "vm-796",
+    "name" : dst_vm_name,
+    "source" : src_vm_id,
     "placement" : {
-        "folder" : "group-v779",
-        "datastore" : "datastore-707",
-        "host" : "host-9",
-        "resource_pool": "resgroup-292"
+        "folder" : folder_id,
+        "datastore" : datastore_id,
+        "host" : host_id,
+        "resource_pool": resource_pool_id
     }
 }
 
+print (json_data)
+
+"""
 response = requests.post(url=clone_url, headers={"vmware-api-session-id": session_id}, json=json_data, verify=False)
 if response.status_code == 200:
     print ("VM cloned successfully")
 else:
     print (f"VM clone failed : {response.text}")
+
+"""
